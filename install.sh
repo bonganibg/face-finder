46710e747b6caae478890cc2077533f6f55a8720
Bongani Gumbo <88882571+bonganibg@users.noreply.github.com>
2024-10-30 16:08:24 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,3 +16,12 @@ apt install python3-venv -y
 echo "install stuff"
 apt install libgl1-mesa-glx
 
+echo "create virtual environment"
+python3 -m venv .venv
+source .venv/bin/activate
+
+echo "install dependencies"
+pip install -r requirements.txt
+
+echo "run application"
+python3 main.py
